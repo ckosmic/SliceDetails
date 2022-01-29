0572ea1f3d99d7dfdc1c192abc607399f5a13207
Christian Kosman <christiankosman@gmail.com>
2022-01-29 02:53:53 -0800

    Remove SiraLog from SliceRecorder

--- SliceDetails/SliceRecorder.cs
+++ SliceDetails/SliceRecorder.cs
@@ -11,13 +11,11 @@ namespace SliceDetails
 	{
 		private readonly BeatmapObjectManager _beatmapObjectManager;
 		private readonly SliceProcessor _sliceProcessor;
-		private readonly SiraLog _siraLog;
 
 		private Dictionary<ISaberSwingRatingCounter, NoteInfo> _noteSwingInfos = new Dictionary<ISaberSwingRatingCounter, NoteInfo>();
 		private List<NoteInfo> _noteInfos = new List<NoteInfo>();
 
-		public SliceRecorder(SiraLog siraLog, BeatmapObjectManager beatmapObjectManager, SliceProcessor sliceProcessor) {
-			_siraLog = siraLog;
+		public SliceRecorder(BeatmapObjectManager beatmapObjectManager, SliceProcessor sliceProcessor) {
 			_beatmapObjectManager = beatmapObjectManager;
 			_sliceProcessor = sliceProcessor;
 		}
@@ -55,7 +53,6 @@ namespace SliceDetails
 
 			Vector2 cutDirection = new Vector3(-noteCutInfo.cutNormal.y, noteCutInfo.cutNormal.x);
 			float cutAngle = Mathf.Atan2(cutDirection.y, cutDirection.x) * Mathf.Rad2Deg + 180f;
-			_siraLog.Info(cutAngle);
 
 			float cutOffset = noteCutInfo.cutDistanceToCenter;
 			Vector3 noteCenter = noteController.noteTransform.position;
